44ab6834b93a589763b216a2373e06cf5d583280
Tim <s87462830@gmail.com>
2021-09-13 23:14:14 +0800

    feat: add ehandlebars & add index page

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 //require package in the project
 const express = require('express')
+const exhbs = require('express-handlebars')
 
 const app = express()
 const PORT = 3000
 
+//Setting: template engine
+app.engine('hbs', exhbs({
+  defaultLayout: 'main',
+  extname: 'hbs',
+}))
+app.set('view engine', 'hbs')
+
 //Routes: index page
 app.get('/', (req,res) => {
-  res.send('test')
+  res.render('index')
 })
 
 //Activate and listening on the express server
