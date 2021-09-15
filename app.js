af1149fd587a474aea01a0c12ab82771ccc27c4b
Tim <s87462830@gmail.com>
2021-09-15 20:41:20 +0800

    feat: add mongoose & add users seeder

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 //require package in the project
 const express = require('express')
 const exhbs = require('express-handlebars')
+const mongoose = require('mongoose')
+const users = require('./models/users')
 
 const app = express()
 const PORT = 3000
+
+//Setting: database mongodb
+mongoose.connect('mongodb://localhost/users', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+db.once('open', () => {
+  console.log('mongodb is connected!')
+})
 
 //Setting: template engine
 app.engine('hbs', exhbs({
@@ -15,6 +28,10 @@ app.set('view engine', 'hbs')
 //Routes: index page
 app.get('/', (req,res) => {
   res.render('index')
+})
+
+//Routes: catch login data at index page
+app.post('/', (req,res) => {
 })
 
 //Activate and listening on the express server
